3bfb1730c0bbd1b1adaf8e837ed2fa0e2b922fea
Tero Keränen <tero-keranen@hotmail.com>
2023-01-25 21:17:20 +0200

    Debugging fetch

--- app.js
+++ app.js
@@ -34,62 +34,71 @@ app.post("/businessid", async (req,res) => {
     
     // get data 
     async function getData(url) {
-        
-        try {
-            
 
-            let fetch_resp = await fetch(url)
-            let json = await fetch_resp.json();
-            
-            let x = json.results;
-            let companyName;
-            let id;
-            let businessLines;
-            
-            let wholeAddress;
 
-            x.forEach((item) => {
-                if (item.businessLines.length <= 0){
-                    businessLines = "businessLines tietoja ei saatavilla"
-                }
-                
-                companyName = item.name; // Save companys name to variable
-                id = item.businessId; // save companys id to variable
-                let i = item.addresses // Take adresses to own variable so you can iterate those
-                
-                i.forEach((addres) => {
-                    let city; 
-                    let street;
-                    let postcode;
-
-                    for (const [key,value] of Object.entries(addres)) {
-
-                        // check if key is what whe want and save it into variable
-                        if(key === "street"){
-                            street = value
-                        } 
-                        // check if key is what whe want and save it into variable
-                        if (key === "postCode") {
-                            postcode = value
-                        }
-                        // check if key is what whe want and save it into variable
-                        if (key === "city") {
-                            city = value;
-                        }
+            await fetch(url)
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        console.error(response.status, response.statusText)
                     }
-                    // save iterated address informations to own variable so we can pass whole address to website 
-                    wholeAddress = `${street}, ${city}, ${postcode}`
                 })
-                
-                
-            })
-            res.render('index', {name:companyName, address: wholeAddress, bussline: businessLines })
+                .then((data) => {
+                    console.log(data.results);
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+
+            // let fetch_resp = await fetch(url)
+            // let json = await fetch_resp.json();
             
-        } catch (error) {
+            // let x = json.results;
+            // let companyName;
+            // let id;
+            // let businessLines;
             
+            // let wholeAddress;
+
+            // x.forEach((item) => {
+            //     if (item.businessLines.length <= 0){
+            //         businessLines = "businessLines tietoja ei saatavilla"
+            //     }
                 
-            res.render('index', {errorMsg: "Ei löydy" })
-        }
+            //     companyName = item.name; // Save companys name to variable
+            //     id = item.businessId; // save companys id to variable
+            //     let i = item.addresses // Take adresses to own variable so you can iterate those
+                
+            //     i.forEach((addres) => {
+            //         let city; 
+            //         let street;
+            //         let postcode;
+
+            //         for (const [key,value] of Object.entries(addres)) {
+
+            //             // check if key is what whe want and save it into variable
+            //             if(key === "street"){
+            //                 street = value
+            //             } 
+            //             // check if key is what whe want and save it into variable
+            //             if (key === "postCode") {
+            //                 postcode = value
+            //             }
+            //             // check if key is what whe want and save it into variable
+            //             if (key === "city") {
+            //                 city = value;
+            //             }
+            //         }
+            //         // save iterated address informations to own variable so we can pass whole address to website 
+            //         wholeAddress = `${street}, ${city}, ${postcode}`
+            //     })
+                
+                
+            // })
+            res.render('index', {name:"companyName", address: "wholeAddress", bussline: "businessLines" })
+            
+        
         
     }
     
